a68eab519606f74e31c3dd2acf057deb36fad718
StartupGeorgia <info@startupgeorgia.org>
2026-05-17 13:34:12 +0400

    ci: deploy nika-giorgadze to /warehouse

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@electric-sql/pglite"],
+  basePath: '/warehouse',
+  assetPrefix: '/warehouse',
 };
 
 export default nextConfig;
